90b49e3dc59b162eb8f05161b0f8571b894111e8
bgaboo <gabor.bauer@live.com>
2015-04-22 09:07:16 +0200

    Initial commit

---
+++ ImmutableSerializationPOC/ImmutableSerializationPOC/ImmutableClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ImmutableSerializationPOC
+{
+    [DataContract]
+    public class ImmutableClass
+    {
+        [DataMember(Name = "field1_public")]
+        private string _property;
+        public string Property { get { return _property; } set { _property = value; } }
+
+        [DataMember(Name = "field2_noSetter")]
+        private string _immutableProperty;
+        // DEVNOTE: property has no setter, DataMember attribute should be on the private field
+        //[DataMember(Name = "field2_noSetter")]
+        public string ImmutableProperty { get { return _immutableProperty; } }
+
+        [DataMember(Name = "field23_privateSetter")]
+        public string ImmutablePrivateSetterProperty { get; private set; }
+
+        private ImmutableClass()
+        {
+        }
+
+        public ImmutableClass(string property, string immutableProperty, string immutablePrivateSetterProperty)
+        {
+            _property = property;
+            _immutableProperty = immutableProperty;
+            ImmutablePrivateSetterProperty = immutablePrivateSetterProperty;
+        }
+    }
+}
---
+++ ImmutableSerializationPOC/ImmutableSerializationPOC/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ImmutableSerializationPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImmutableClass test = new ImmutableClass("name", "immutable", "private setter");
+
+            //string serializedXml = XmlSerialize<ImmutableClass>(test);
+            //Console.WriteLine(string.Format("XML serialized object: {0}", serializedXml));
+            //ImmutableClass deserializedXml = null;
+            //deserializedXml = XmlDeserialize<ImmutableClass>(serializedXml);
+
+            //string serializedDC = DataContractSerialize<ImmutableClass>(test);
+            //Console.WriteLine(string.Format("DataContract serialized object: {0}", serializedDC));
+
+            string serializedNDC = NetDataContractSerialize<ImmutableClass>(test);
+            Console.WriteLine(string.Format("NetDataContract serialized object: {0}", serializedNDC));
+            ImmutableClass deserializedNDC = null;
+            deserializedNDC = NetDataContractDeserialize<ImmutableClass>(serializedNDC);
+
+            Console.ReadLine();
+        }
+        
+        public static string NetDataContractSerialize<T>(T value)
+        {
+            if (value == null)
+                return null;
+
+            try
+            {
+                NetDataContractSerializer serializer = new NetDataContractSerializer();
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, value);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    return new StreamReader(stream).ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private static T NetDataContractDeserialize<T>(string serialized)
+        {
+            try
+            {
+                NetDataContractSerializer serializer = new NetDataContractSerializer();
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (TextWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(serialized);
+                        writer.Flush();
+                        stream.Position = 0;
+                        return (T)serializer.Deserialize(stream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default(T);
+            }
+        }
+
+        public static string XmlSerialize<T>(T value)
+        {
+            if (value == null)
+                return null;
+
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                using (StringWriter stringWriter = new StringWriter())
+                {
+                    using (XmlWriter writer = XmlWriter.Create(stringWriter))
+                    {
+                        xmlSerializer.Serialize(writer, value);
+                        return stringWriter.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private static T XmlDeserialize<T>(string serialized)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                return (T)xmlSerializer.Deserialize(new StringReader(serialized));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default(T);
+            }
+        }
+    }
+}
