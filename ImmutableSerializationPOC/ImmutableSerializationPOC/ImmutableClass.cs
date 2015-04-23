b2595ead4a87a0f2d53794118f06344548826691
bgaboo <gabor.bauer@live.com>
2015-04-23 09:27:58 +0200

    Comparison of serializers

--- ImmutableSerializationPOC/ImmutableSerializationPOC/ImmutableClass.cs
+++ ImmutableSerializationPOC/ImmutableSerializationPOC/ImmutableClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -29,6 +30,56 @@ namespace ImmutableSerializationPOC
         }
 
         public ImmutableClass(string property, string immutableProperty, string immutablePrivateSetterProperty)
+        {
+            _property = property;
+            _immutableProperty = immutableProperty;
+            ImmutablePrivateSetterProperty = immutablePrivateSetterProperty;
+        }
+    }
+
+    [DataContract]
+    public class ImmutableClassSS : ImmutableClass
+    {
+        private string _property;
+        [DataMember(Name = "field1_public")]
+        public string Property { get { return _property; } set { _property = value; } }
+
+        
+        private string _immutableProperty;
+        [DataMember(Name = "field2_noSetter")]
+        public string ImmutableProperty { get { return _immutableProperty; } }
+
+        [DataMember(Name = "field23_privateSetter")]
+        public string ImmutablePrivateSetterProperty { get; private set; }
+        
+        public ImmutableClassSS(string property, string immutableProperty, string immutablePrivateSetterProperty) : base (property, immutableProperty, immutablePrivateSetterProperty)
+        {
+            _property = property;
+            _immutableProperty = immutableProperty;
+            ImmutablePrivateSetterProperty = immutablePrivateSetterProperty;
+        }
+    }
+
+    [ProtoContract]
+    public class ImmutableClassProto// : ImmutableClass
+    {
+        [ProtoMember(1)]
+        private string _property;
+        public string Property { get { return _property; } set { _property = value; } }
+
+        [ProtoMember(2)]
+        private string _immutableProperty;
+        public string ImmutableProperty { get { return _immutableProperty; } }
+
+        [ProtoMember(3)]
+        public string ImmutablePrivateSetterProperty { get; private set; }
+
+        private ImmutableClassProto()
+        { 
+        }
+
+        public ImmutableClassProto(string property, string immutableProperty, string immutablePrivateSetterProperty)
+            //: base(property, immutableProperty, immutablePrivateSetterProperty)
         {
             _property = property;
             _immutableProperty = immutableProperty;
--- ImmutableSerializationPOC/ImmutableSerializationPOC/Program.cs
+++ ImmutableSerializationPOC/ImmutableSerializationPOC/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -14,7 +16,13 @@ namespace ImmutableSerializationPOC
     {
         static void Main(string[] args)
         {
+            string keyNDC = "NetDataContract";
+            string keyNJ = "NewtonSoftJson";
+            string keyP = "ProtoBuff";
+
             ImmutableClass test = new ImmutableClass("name", "immutable", "private setter");
+            ImmutableClassSS test2 = new ImmutableClassSS("name", "immutable", "private setter");
+            ImmutableClassProto test3 = new ImmutableClassProto("name", "immutable", "private setter");
 
             //string serializedXml = XmlSerialize<ImmutableClass>(test);
             //Console.WriteLine(string.Format("XML serialized object: {0}", serializedXml));
@@ -29,9 +37,199 @@ namespace ImmutableSerializationPOC
             ImmutableClass deserializedNDC = null;
             deserializedNDC = NetDataContractDeserialize<ImmutableClass>(serializedNDC);
 
+            Console.WriteLine();
+
+            string serializedNJ = NewtonSoftJsonSerialize<ImmutableClass>(test);
+            Console.WriteLine(string.Format("NewtonSoft JSON serialized object: {0}", serializedNJ));
+            ImmutableClass deserializedNJ = null;
+            deserializedNJ = NewtonSoftJsonDeserialize<ImmutableClass>(serializedNJ);
+
+            Console.WriteLine();
+
+            // DEVNOTE: DataMember attribuites on private fields did not work for ServiceStack JSON serializer
+            // it also does not work with properties without setter and does not work with private members
+
+            string serializedSJ = ServiceStackJsonSerialize<ImmutableClassSS>(test2);
+            Console.WriteLine(string.Format("ServiceStack JSON serialized object: {0}", serializedSJ));
+            ImmutableClassSS deserializedSJ = null;
+            deserializedSJ = ServiceStackJsonDeserialize<ImmutableClassSS>(serializedSJ);
+
+            Console.WriteLine();
+
+            string serializedP = ProtoSerialize<ImmutableClassProto>(test3);
+            Console.WriteLine(string.Format("ProtoBuf serialized object: {0}", serializedP));
+            ImmutableClassProto deserializedP = null;
+            deserializedP = ProtoDeserialize<ImmutableClassProto>(serializedP);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Dictionary<string, List<long>> resultsSerialize = new Dictionary<string, List<long>>();
+            resultsSerialize.Add(keyNDC, new List<long>());
+            resultsSerialize.Add(keyNJ, new List<long>());
+            resultsSerialize.Add(keyP, new List<long>());
+
+            Dictionary<string, List<long>> resultsDeserialize = new Dictionary<string, List<long>>();
+            resultsDeserialize.Add(keyNDC, new List<long>());
+            resultsDeserialize.Add(keyNJ, new List<long>());
+            resultsDeserialize.Add(keyP, new List<long>());
+
+            Stopwatch sw;
+            for (int i = 0; i < 1000; i++)
+            {
+                // NetDataContract
+                sw = Stopwatch.StartNew();
+                serializedNDC = NetDataContractSerialize<ImmutableClass>(test);
+                sw.Stop();
+                resultsSerialize[keyNDC].Add(sw.ElapsedTicks);
+
+                sw = Stopwatch.StartNew();
+                deserializedNDC = NetDataContractDeserialize<ImmutableClass>(serializedNDC);
+                sw.Stop();
+                resultsDeserialize[keyNDC].Add(sw.ElapsedTicks);
+
+                // NewtonSoftJson
+                sw = Stopwatch.StartNew();
+                serializedNJ = NewtonSoftJsonSerialize<ImmutableClass>(test);
+                sw.Stop();
+                resultsSerialize[keyNJ].Add(sw.ElapsedTicks);
+
+                sw = Stopwatch.StartNew();
+                deserializedNJ = NewtonSoftJsonDeserialize<ImmutableClass>(serializedNJ);
+                sw.Stop();
+                resultsDeserialize[keyNJ].Add(sw.ElapsedTicks);
+
+                // ProtoBuf
+                sw = Stopwatch.StartNew();
+                serializedP = ProtoSerialize<ImmutableClassProto>(test3);
+                sw.Stop();
+                resultsSerialize[keyP].Add(sw.ElapsedTicks);
+
+                sw = Stopwatch.StartNew();
+                deserializedP = ProtoDeserialize<ImmutableClassProto>(serializedP);
+                sw.Stop();
+                resultsDeserialize[keyP].Add(sw.ElapsedTicks);
+            }
+
+            foreach (var item in resultsSerialize)
+            {
+                Console.WriteLine(string.Format("{0} serialization AVG {1} ticks", item.Key, item.Value.Average()));
+            }
+            foreach (var item in resultsDeserialize)
+            {
+                Console.WriteLine(string.Format("{0} deserialization AVG {1} ticks", item.Key, item.Value.Average()));
+            }
+
             Console.ReadLine();
         }
-        
+
+        public static string ProtoSerialize<T>(T value)
+        {
+            if (value == null)
+                return null;
+
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    ProtoBuf.Serializer.Serialize(stream, value);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    return new StreamReader(stream).ReadToEnd();
+                }
+                // XmlWriter
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private static T ProtoDeserialize<T>(string serialized)
+        {
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (TextWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(serialized);
+                        writer.Flush();
+                        stream.Position = 0;
+                        return ProtoBuf.Serializer.Deserialize<T>(stream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default(T);
+            }
+        }
+
+        public static string ServiceStackJsonSerialize<T>(T value)
+        {
+            if (value == null)
+                return null;
+
+            try
+            {
+                return ServiceStack.Text.JsonSerializer.SerializeToString<T>(value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private static T ServiceStackJsonDeserialize<T>(string serialized)
+        {
+            try
+            {
+                return ServiceStack.Text.JsonSerializer.DeserializeFromString<T>(serialized);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default(T);
+            }
+        }
+
+        public static string NewtonSoftJsonSerialize<T>(T value)
+        {
+            if (value == null)
+                return null;
+
+            try
+            {
+                JsonSerializer serializer = JsonSerializer.Create();
+                using (TextWriter writer = new StringWriter())
+                {
+                    serializer.Serialize(writer, value);
+                    return writer.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private static T NewtonSoftJsonDeserialize<T>(string serialized)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(serialized);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default(T);
+            }
+        }
+
         public static string NetDataContractSerialize<T>(T value)
         {
             if (value == null)
